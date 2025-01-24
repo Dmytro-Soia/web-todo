9f18bc6e40982ec8d5657409fc5715edc9cf3c9f
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-01-24 13:26:32 +0100

    added base html, css and creation todo function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
 import './style.css'
 
-console.log('Hello from typescript')
+const input = document.querySelector<HTMLInputElement>("#todo-input")
+const button = document.querySelector<HTMLButtonElement>("#add-todo-button")
+const storage = document.querySelector<HTMLUListElement>("#todo-storage")
+
+
+function fn(input: HTMLInputElement): void {
+    if (storage) {
+        let a = document.createElement("p")
+        a.innerText = input.value
+        a.classList.add("todo-element")
+        storage.append(a)
+    }
+
+
+}
+
+if (button) {
+    button.addEventListener("click", () => {
+        if (input) {
+            fn(input)
+        }
+
+    })
+}
