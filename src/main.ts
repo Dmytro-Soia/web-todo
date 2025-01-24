1d0381fa7731c34d7ae0a6f76ffffbe40eb47407
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-01-24 14:20:41 +0100

    feat: can added new todos by pressing "Enter"

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,27 @@ const input = document.querySelector<HTMLInputElement>('#todo-input')
 const button = document.querySelector<HTMLButtonElement>('#add-todo-button')
 const storage = document.querySelector<HTMLUListElement>('#todo-storage')
 
-function fn(input: HTMLInputElement): void {
+function addTodo(input: HTMLInputElement): void {
   if (storage) {
-    const a = document.createElement('li')
-    a.innerText = input.value
-    a.classList.add('todo-element')
-    storage.append(a)
+    const todo_li = document.createElement('li')
+    todo_li.innerText = input.value
+    todo_li.classList.add('todo-element')
+    storage.append(todo_li)
   }
+}
+
+if (input) {
+  input.addEventListener('keydown', (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      addTodo(input)
+    }
+  })
 }
 
 if (button) {
   button.addEventListener('click', () => {
     if (input) {
-      fn(input)
+      addTodo(input)
     }
   })
 }
