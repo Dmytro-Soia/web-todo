287582ab058ca70c5fe8917e514321fd958e30af
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-01-24 13:41:18 +0100

    chore: code was formated
    close #6

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,22 @@
 import './style.css'
 
-const input = document.querySelector<HTMLInputElement>("#todo-input")
-const button = document.querySelector<HTMLButtonElement>("#add-todo-button")
-const storage = document.querySelector<HTMLUListElement>("#todo-storage")
-
+const input = document.querySelector<HTMLInputElement>('#todo-input')
+const button = document.querySelector<HTMLButtonElement>('#add-todo-button')
+const storage = document.querySelector<HTMLUListElement>('#todo-storage')
 
 function fn(input: HTMLInputElement): void {
-    if (storage) {
-        let a = document.createElement("p")
-        a.innerText = input.value
-        a.classList.add("todo-element")
-        storage.append(a)
-    }
-
-
+  if (storage) {
+    let a = document.createElement('li')
+    a.innerText = input.value
+    a.classList.add('todo-element')
+    storage.append(a)
+  }
 }
 
 if (button) {
-    button.addEventListener("click", () => {
-        if (input) {
-            fn(input)
-        }
-
-    })
+  button.addEventListener('click', () => {
+    if (input) {
+      fn(input)
+    }
+  })
 }
